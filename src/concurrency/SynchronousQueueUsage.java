73db0ba903d577a51fef580a4d0c25e8fdd723cb
Jingguo Yao <yaojingguo@gmail.com>
2014-05-01 10:42:23 +0800

    Up

---
+++ src/concurrency/SynchronousQueueUsage.java
@@ -0,0 +1,35 @@
+
+package concurrency;
+
+import java.util.concurrent.*;
+
+public class SynchronousQueueUsage {
+  public static void main (String [] args) {
+    final SynchronousQueue<Integer> q = new SynchronousQueue<Integer>();
+    Thread putter = new Thread() {
+      @Override
+      public void run() {
+        try {
+          Thread.sleep(10 * 1000);
+          q.put(1);
+          System.out.println("Put success");  
+        } catch (InterruptedException ie) {
+          throw new RuntimeException(ie);
+        }
+      }
+    };
+    Thread taker = new Thread() {
+      @Override
+      public void run() {
+        try {
+          q.take();
+          System.out.println("Take success");  
+        } catch (InterruptedException ie) {
+          throw new RuntimeException(ie);
+        }
+      }
+    };
+    putter.start();
+    taker.start();
+  }
+}
