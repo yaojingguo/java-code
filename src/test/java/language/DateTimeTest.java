5c525fb37720b9bba7fca3c2bdc5f5b34cfa1cb7
Jingguo Yao <yaojingguo@gmail.com>
2015-10-15 13:36:33 +0800

    Update

---
+++ src/test/java/language/DateTimeTest.java
@@ -0,0 +1,64 @@
+package language;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class DateTimeTest {
+
+  @Test
+  public void testLocalDate() {
+    LocalDate now = LocalDate.now();
+    System.out.println(now);
+    System.out.println(now.minusDays(6));
+
+    Map map = new HashMap();
+    map.put("abc", 1.1);
+    map.put("def", "x");
+    System.out.println(map);
+  }
+
+  @Test
+  public void testLocalDateTime() {
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println(now);
+    System.out.println(now.toInstant(ZoneOffset.UTC));
+  }
+
+  @Test
+  public void testNullToString() {
+    System.out.println("abc" + null);
+  }
+
+  public static void a() {
+    System.out.println("a begin");
+    b();
+    System.out.println("a end");
+  }
+
+  private static void b() {
+    System.out.println("b begin");
+    c();
+    System.out.println("b end");
+  }
+
+  private static void c() {
+    System.out.println("c begin");
+    if (true) throw new RuntimeException();
+    System.out.println("c end");
+  }
+
+  public static void main(String[] args) {
+    try {
+      System.out.println("main begin");
+      a();
+      System.out.println("main end");
+    } catch (Throwable th) {
+      throw th;
+    }
+  }
+}
