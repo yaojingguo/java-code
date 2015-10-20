f1a56f90b39ca3e1123234fe78d58f5686723ca0
Jingguo Yao <yaojingguo@gmail.com>
2015-10-20 17:21:10 +0800

    Update

--- src/test/java/io/FileOutputStreamTest.java
+++ src/test/java/io/FileOutputStreamTest.java
@@ -7,11 +7,16 @@ import org.junit.Test;
 
 public class FileOutputStreamTest {
 
-  private String pathname = "data1.txt";
+  private String pathname1 = "data1.txt";
+  private String pathname2 = "data2.txt";
 
   @Test
   public void testAppend() throws IOException {
-    try (FileOutputStream out = new FileOutputStream(pathname, false)) {
+    try (FileOutputStream out = new FileOutputStream(pathname1, false)) {
+      out.write(0xFF & 'x');
+    }
+    
+    try (FileOutputStream out = new FileOutputStream(pathname2, true)) {
       out.write(0xFF & 'x');
     }
   }
