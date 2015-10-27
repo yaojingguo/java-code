e25578ce2b69abd2402e5565a8af2b7655007d38
Jingguo Yao <yaojingguo@gmail.com>
2015-10-27 22:51:53 +0800

    Update

---
+++ src/main/java/OutOfMemory.java
@@ -0,0 +1,11 @@
+// Use `java -XshowSettings:all` to show heap size settings 
+public class OutOfMemory {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Object[] table = new Object[32];
+    int _1G = 1024 * 1024 * 1024;
+    for (int i = 0; i < 2; i++) {
+      table[i] = new byte[_1G];
+    }
+  }
+}
---
+++ src/test/java/language/MgmtTest.java
@@ -0,0 +1,19 @@
+package language;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MgmtTest {
+
+  @Test
+  public void testXXX() {
+
+    
+
+
+  }
+
+}
