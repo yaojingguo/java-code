316631a4a983e5e701f618ab08ad78d6e7f61d0b
Jingguo Yao <yaojingguo@gmail.com>
2015-11-02 11:01:42 +0800

    Update

--- src/main/java/concurrency/ThreadInterrupt.java
+++ src/main/java/concurrency/ThreadInterrupt.java
@@ -1,44 +1,44 @@
 package concurrency;
 
 public class ThreadInterrupt {
-    static class Worker1 extends Thread {
-        public void run() {
-            try {
-                work();
-            } catch (InterruptedException e) {
-                System.out.println("interrupt status: " + isInterrupted());    
-                e.printStackTrace();
-            }
-        }
-        private void work() throws InterruptedException {
-            for (;;) 
-                if (interrupted())
-                    throw new InterruptedException();
-        }
-    }
-    static class Worker2 extends Thread {
-        public void run() {
-            try {
-                work();
-            } catch (InterruptedException e) {
-                System.out.println("interrupt status: " + isInterrupted());    
-                e.printStackTrace();
-            }
-        }
-        private void work() throws InterruptedException {
-            for (;;) 
-                if (isInterrupted())
-                    throw new InterruptedException();
-        }
+  static class Worker1 extends Thread {
+    public void run() {
+      try {
+        work();
+      } catch (InterruptedException e) {
+        System.out.println("interrupt status: " + isInterrupted());
+        e.printStackTrace();
+      }
     }
 
-    public static void main (String [] args) {
-        Thread t;
-        t = new Worker1();
-        t.start();
-        t.interrupt();
-        t = new Worker2();
-        t.start();
-        t.interrupt();
+    private void work() throws InterruptedException {
+      for (;;)
+        if (interrupted()) throw new InterruptedException();
     }
+  }
+  static class Worker2 extends Thread {
+    public void run() {
+      try {
+        work();
+      } catch (InterruptedException e) {
+        System.out.println("interrupt status: " + isInterrupted());
+        e.printStackTrace();
+      }
+    }
+
+    private void work() throws InterruptedException {
+      for (;;)
+        if (isInterrupted()) throw new InterruptedException();
+    }
+  }
+
+  public static void main(String[] args) {
+    Thread t;
+    t = new Worker1();
+    t.start();
+    t.interrupt();
+    t = new Worker2();
+    t.start();
+    t.interrupt();
+  }
 }
