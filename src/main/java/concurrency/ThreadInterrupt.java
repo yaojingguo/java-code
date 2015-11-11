488bc39d07c918fab79d3e6abf2741fc08c5c36a
Jingguo Yao <yaojingguo@gmail.com>
2015-11-11 14:45:23 +0800

    Update

--- src/main/java/concurrency/ThreadInterrupt.java
+++ src/main/java/concurrency/ThreadInterrupt.java
@@ -12,10 +12,11 @@ public class ThreadInterrupt {
     }
 
     private void work() throws InterruptedException {
-      for (;;)
-        if (interrupted()) throw new InterruptedException();
+      for (;;);
+      // if (interrupted()) throw new InterruptedException();
     }
   }
+
   static class Worker2 extends Thread {
     public void run() {
       try {
@@ -32,13 +33,31 @@ public class ThreadInterrupt {
     }
   }
 
-  public static void main(String[] args) {
-    Thread t;
-    t = new Worker1();
-    t.start();
-    t.interrupt();
-    t = new Worker2();
-    t.start();
-    t.interrupt();
+  static class Sleeper extends Thread {
+    @Override
+    public void run() {
+      try {
+        Thread.sleep(10 * 60 * 1000);
+      } catch (InterruptedException itEx) {
+        System.out.println("Interrupted when sleeping");
+        throw new RuntimeException(itEx);
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+//    Thread t;
+//    t = new Worker1();
+//    t.start();
+//    t.interrupt();
+    
+    Sleeper sleeper = new Sleeper();
+    sleeper.start();
+    Thread.sleep(100);
+    sleeper.interrupt();
+
+    // t = new Worker2();
+    // t.start();
+    // t.interrupt();
   }
 }
--- src/test/java/language/StringTest.java
+++ src/test/java/language/StringTest.java
@@ -1,16 +1,27 @@
 package language;
 
-import org.junit.Test;
-
 import static com.google.common.truth.Truth.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 public class StringTest {
-  
+
   @Test
   public void testLowerCase() {
     assertThat("Φ".toUpperCase()).isEqualTo("Φ");
-//    assertThat("ф".toUpperCase()).isEqualTo("Φ");
     assertThat("φ".toUpperCase()).isEqualTo("Φ");
+  }
+
+
+  @Test
+  public void testFormat() {
+    List list = new ArrayList();
+    list.add("item1");
+    String msg = String.format("1: %s, 2: %s, 3: %s", "one", "two", list);
+    System.out.println(msg);
   }
 
 }
