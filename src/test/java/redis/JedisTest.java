20e40fac2333317f0c95c697ad6c29ee536d0c34
Jingguo Yao <yaojingguo@gmail.com>
2015-12-02 14:58:12 +0800

    Update

--- src/test/java/redis/JedisTest.java
+++ src/test/java/redis/JedisTest.java
@@ -33,7 +33,7 @@ public class JedisTest {
   }
 
   @Test
-  public void first() {
+  public void testJedis() {
     Jedis jedis = new Jedis(host);
     jedis.set("foo", "bar");
     String value = jedis.get("foo");
@@ -41,7 +41,7 @@ public class JedisTest {
   }
 
   @Test
-  public void second() {
+  public void testPool() {
     verifyPool(pool);
   }
 
@@ -55,39 +55,5 @@ public class JedisTest {
       Set<String> sose = jedis.zrange("sose", 0, -1);
       assertThat(sose).containsExactly("car", "bike");
     }
-  }
-
-  @Test
-  public void third() {
-    Set sentinels = new HashSet();
-    sentinels.add(new HostAndPort("10.202.80.117", 17500).toString());
-    sentinels.add(new HostAndPort("10.202.80.118", 17500).toString());
-    JedisSentinelPool sentinelPool = new JedisSentinelPool("master7500", sentinels, "zt1116@ops");
-    System.out.println("Current master: " + sentinelPool.getCurrentHostMaster().toString());
-    verifyPool(sentinelPool);
-  }
-
-
-  @Test
-  public void testFailOver() {
-    Set sentinels = new HashSet();
-    sentinels.add(new HostAndPort("127.0.0.1", 26379).toString());
-    JedisSentinelPool sentinelPool = new JedisSentinelPool("mymaster", sentinels);
-    System.out.println("Current master: " + getMaster(sentinelPool));
-    verifyPool(sentinelPool);
-  }
-
-  private void handleFailOver(JedisSentinelPool pool) {
-    for (;;) {
-      try {
-        verifyPool(pool);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  private String getMaster(JedisSentinelPool pool) {
-    return pool.getCurrentHostMaster().toString();
   }
 }
--- src/test/java/redis/RedisSentinelTest.java
+++ src/test/java/redis/RedisSentinelTest.java
@@ -1,5 +1,47 @@
 package redis;
 
-public class RedisSentinelTest {
+import static redis.JedisTest.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisSentinelPool;
+
+public class RedisSentinelTest {
+  @Test
+  public void third() {
+    Set sentinels = new HashSet();
+    sentinels.add(new HostAndPort("10.202.80.117", 17500).toString());
+    sentinels.add(new HostAndPort("10.202.80.118", 17500).toString());
+    JedisSentinelPool sentinelPool = new JedisSentinelPool("master7500", sentinels, "zt1116@ops");
+    System.out.println("Current master: " + sentinelPool.getCurrentHostMaster().toString());
+    verifyPool(sentinelPool);
+  }
+
+
+  @Test
+  public void testFailOver() {
+    Set sentinels = new HashSet();
+    sentinels.add(new HostAndPort("127.0.0.1", 26379).toString());
+    JedisSentinelPool sentinelPool = new JedisSentinelPool("mymaster", sentinels);
+    System.out.println("Current master: " + getMaster(sentinelPool));
+    verifyPool(sentinelPool);
+  }
+
+  private void handleFailOver(JedisSentinelPool pool) {
+    for (;;) {
+      try {
+        verifyPool(pool);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private String getMaster(JedisSentinelPool pool) {
+    return pool.getCurrentHostMaster().toString();
+  }
 }
