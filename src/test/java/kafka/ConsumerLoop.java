a8a81d5d71c9f31627c33920678f35bd7d6b6ed5
Jingguo Yao <yaojingguo@gmail.com>
2016-03-11 14:22:46 +0800

    Update

--- src/test/java/kafka/ConsumerLoop.java
+++ src/test/java/kafka/ConsumerLoop.java
@@ -1,9 +1,14 @@
 package kafka;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -20,7 +25,7 @@ public class ConsumerLoop implements Runnable {
     this.id = id;
     this.topics = topics;
     Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
+    props.put("bootstrap.servers", "knode-1:9092");
     props.put("group.id", groupId);
     props.put("key.deserializer", StringDeserializer.class.getName());
     props.put("value.deserializer", StringDeserializer.class.getName());
@@ -34,11 +39,11 @@ public class ConsumerLoop implements Runnable {
       while (true) {
         ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
         for (ConsumerRecord<String, String> record : records) {
-          Map<String, Object> data = new HashMap<>();
-          data.put("partition", record.partition());
-          data.put("offset", record.offset());
-          data.put("value", record.value());
-          System.out.println(this.id + ": " + data);
+          Map<String, Object> map = new HashMap<>();
+          map.put("partition", record.partition());
+          map.put("offset", record.offset());
+          map.put("value", record.value());
+          System.out.println(this.id + ": " + map);
         }
       }
     } catch (WakeupException e) {
@@ -50,5 +55,34 @@ public class ConsumerLoop implements Runnable {
 
   public void shutdown() {
     consumer.wakeup();
+  }
+  
+  public static void main(String[] args) { 
+    int numConsumers = 3;
+    String groupId = "consumer-tutorial-group";
+    List<String> topics = Arrays.asList("consumer-tutorial");
+    ExecutorService executor = Executors.newFixedThreadPool(numConsumers);
+
+    final List<ConsumerLoop> consumers = new ArrayList<>();
+    for (int i = 0; i < numConsumers; i++) {
+      ConsumerLoop consumer = new ConsumerLoop(i, groupId, topics);
+      consumers.add(consumer);
+      executor.submit(consumer);
+    }
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        for (ConsumerLoop consumer : consumers) {
+          consumer.shutdown();
+        } 
+        executor.shutdown();
+        try {
+          executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
   }
 }
