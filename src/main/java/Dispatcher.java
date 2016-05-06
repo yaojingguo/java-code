6afe9fa33f3b32d620a140909a34285e3b594403
Jingguo Yao <yaojingguo@gmail.com>
2016-05-06 14:36:59 +0800

    Update

--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -1,63 +1,96 @@
-class Processor extends Thread {
-    private Status status;
+import java.util.concurrent.ThreadLocalRandom;
 
-    public Processor(Status status) {
-        this.status = status;
+class Processor extends Thread {
+    private Cond cond;
+    private int seconds;
+
+    public Processor(Cond status, int seconds, int no) {
+        this.cond = status;
+        this.seconds = seconds;
+        setName("processor-" + no);
     }
 
     @Override
     public void run() {
         try {
-            // TODO: is log4j.info thread safe?. 
-            // TODO: access the update state of status.count in a thread-safe way
-            System.out.printf("running %s\n with count=", getName(), status.count);
-            // TODO: use random sleep value
-            Thread.sleep(2 * 1000);
+            // The output might be scrambled since no coordination is used here.
+            System.out.printf("running %s\n", getName());
+            Thread.sleep(seconds * 1000);
         } catch (InterruptedException ie) {
             throw new RuntimeException(ie);
         } finally {
-            synchronized (status.monitor) {
-                status.count--;
-                status.monitor.notify();
-            }
+            cond.release();
         }
     }
 }
 
+class Cond {
+    private int limit;
+    private int count;
 
-class Status {
-    // TODO: method to update limit value
-    public int limit;
-    public int count;
-    public final Object monitor;
-
-    public Status(int limit) {
+    public Cond(int limit) {
+        this.count = 0;
         this.limit = limit;
-        this.monitor = new Object();
     }
-    // TODO: toString method
-}
 
+    public int acquire() throws InterruptedException {
+        synchronized (this) {
+            while (count >= limit)
+                wait();
+            return count++;
+        }
+    }
+
+    public void release() {
+        synchronized (this) {
+            count--;
+            notify();
+        }
+    }
+    
+    public void reset(int limit) {
+        synchronized (this) {
+            int oldLimit = this.limit;
+            this.limit = limit;
+            notify();
+            System.out.printf("limit updated from %d to %d\n", oldLimit, limit);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "condition{count: " + count + ", limit: " + limit + "}";
+    }
+}
 
 public class Dispatcher {
+    public static void main(String[] args) throws InterruptedException {
+        final Cond cond = new Cond(5);
 
-    public static void main(String[] args) {
-        Status status = new Status(10);
-        // TODO: more printf here
-        // TODO: 
-        for (;;) {
-            synchronized (status.monitor) {
-                while (status.count >= status.limit)
-                    try {
-                        status.monitor.wait();
-                    } catch (InterruptedException ie) {
-                        throw new RuntimeException(ie);
-                    }
-                status.count++;
+        // Invoke reset after some time
+        Thread operator = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5 * 1000);
+                } catch (InterruptedException ie) {
+                    throw new RuntimeException(ie);
+                }
+                cond.reset(100);
             }
-            // TODO: the overhead to create a thread is a not a big deal.
-            Processor processor = new Processor(status);
+        };
+        operator.start();
+
+        for (;;) {
+            int no = cond.acquire();
+            // If the overhead to create a thread is a concern, a thread pool can be used.
+            Processor processor = new Processor(cond, nextInt(5), no);
             processor.start();
         }
+    }
+
+    // Random int in the range [1, max]
+    private static int nextInt(int max) {
+        return ThreadLocalRandom.current().nextInt(max) + 1;
     }
 }
