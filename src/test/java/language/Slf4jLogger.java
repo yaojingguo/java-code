dbd47c3915076e2a3a1a049e4b60bc52abc935b0
Jingguo Yao <yaojingguo@gmail.com>
2019-09-14 21:58:21 +0800

    update

--- src/test/java/language/Slf4jLogger.java
+++ src/test/java/language/Slf4jLogger.java
@@ -1,17 +1,18 @@
 package language;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 public class Slf4jLogger {
   
-  private Logger log = LoggerFactory.getLogger(getClass());
+  private static Logger log = Logger.getLogger(Slf4jLogger.class);
   
-  @Test
-  public void test() {
+  public static void main(String[] args) {
     log.error("error message");
     log.info("info message");
+    System.out.printf("go");
   }
 
 }
