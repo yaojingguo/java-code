7595da4c60ad8899fed97efd0213f716091955cb
Jingguo Yao <yaojingguo@gmail.com>
2019-10-07 20:32:37 +0800

    update

--- src/main/java/concurrency/CyclicBarrierExamples.java
+++ src/main/java/concurrency/CyclicBarrierExamples.java
@@ -1,19 +1,25 @@
-package algorithm;
+package concurrency;
 
 // JAVA program to demonstrate execution on Cyclic Barrier
+
+import com.google.common.base.Verify;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.Random;
 
 class Computation1 implements Runnable {
-  public static int product = 0;
+  public int product = 0;
 
   public void run() {
-    product = 2 * 3;
     try {
+      System.out.println("computing product");
+      Thread.sleep(1000 + new Random().nextInt(1000));
+      product = 2 * 3;
       CyclicBarrierExamples.newBarrier.await();
+      System.out.println("barrier crossed");
     } catch (InterruptedException | BrokenBarrierException e) {
       e.printStackTrace();
     }
@@ -21,20 +27,19 @@ class Computation1 implements Runnable {
 }
 
 class Computation2 implements Runnable {
-  public static int sum = 0;
+  public int sum = 0;
 
   public void run() {
-    // check if newBarrier is broken or not
-    System.out.println("Is the barrier broken? - " + CyclicBarrierExamples.newBarrier.isBroken());
-    sum = 10 + 20;
+    Verify.verify(!CyclicBarrierExamples.newBarrier.isBroken(), "The barrier is broken");
     try {
+      System.out.println("computing sum");
+      Thread.sleep(1000 + new Random().nextInt(1000));
+      sum = 10 + 20;
       CyclicBarrierExamples.newBarrier.await(3000, TimeUnit.MILLISECONDS);
-
-      // number of parties waiting at the barrier
-      System.out.println(
-          "Number of parties waiting at the barrier "
-              + "at this point = "
-              + CyclicBarrierExamples.newBarrier.getNumberWaiting());
+      System.out.println("barrier crossed");
+      Verify.verify(
+          CyclicBarrierExamples.newBarrier.getNumberWaiting() == 0,
+          "Number of parties waiting at the barrier should be 0");
     } catch (InterruptedException | BrokenBarrierException e) {
       e.printStackTrace();
     } catch (TimeoutException e) {
@@ -55,13 +60,24 @@ public class CyclicBarrierExamples implements Runnable {
   }
 
   public void run() {
+    oneTime();
+
+    // Resetting the newBarrier
+    newBarrier.reset();
+    System.out.println("----------------------------------------");
+
+    oneTime();
+  }
+
+  private void oneTime() {
     System.out.println(
         "Number of parties required to trip the barrier = " + newBarrier.getParties());
-    System.out.println("Sum of product and sum = " + (Computation1.product + Computation2.sum));
+
 
     // objects on which the child thread has to run
     Computation1 comp1 = new Computation1();
     Computation2 comp2 = new Computation2();
+    Verify.verify(comp1.product + comp2.sum == 0);
 
     // creation of child thread
     Thread t1 = new Thread(comp1);
@@ -79,10 +95,6 @@ public class CyclicBarrierExamples implements Runnable {
 
     // barrier breaks as the number of thread waiting for the barrier
     // at this point = 3
-    System.out.println("Sum of product and sum = " + (Computation1.product + Computation2.sum));
-
-    // Resetting the newBarrier
-    newBarrier.reset();
-    System.out.println("Barrier reset successful");
+    Verify.verify(comp1.product + comp2.sum == 36);
   }
 }
