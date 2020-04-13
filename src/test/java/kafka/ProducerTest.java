8b066da3f9fcd9209890ddccdfff583424cf3fd5
Jingguo Yao <yaojingguo@gmail.com>
2020-04-13 23:21:23 +0800

    update

--- src/test/java/kafka/ProducerTest.java
+++ src/test/java/kafka/ProducerTest.java
@@ -2,9 +2,14 @@ package kafka;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Future;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -15,18 +20,53 @@ public class ProducerTest {
 
   public static String topic = "apiTopic";
 
-  private static KafkaProducer<String, String> createProducer() throws UnknownHostException {
-    Properties config = new Properties();
-    config.put("client.id", InetAddress.getLocalHost().getHostName());
-    config.put("bootstrap.servers", "knode-1:9092");
-    config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    return new KafkaProducer<>(config);
+  private static Properties config() throws Exception {
+    Properties cfg = new Properties();
+    cfg.put("client.id", InetAddress.getLocalHost().getHostName());
+    // 10.201.60.78
+    //    cfg.put("bootstrap.servers", "10.201.60.78:9092");
+    cfg.put("bootstrap.servers", "localhost:9092");
+    cfg.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    cfg.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    return cfg;
+  }
+
+  private static KafkaProducer<String, String> createProducer() throws Exception {
+    return new KafkaProducer<>(config());
+  }
+
+  // One run returns 9740ms for creating 2k topics.
+  @Test
+  public void testCreateTopics() throws Exception {
+    List<NewTopic> topics = new ArrayList<>();
+    int count = 2000;
+    for (int i = 0; i < count; i++) {
+      String topicName = String.format("2-k-%d", i);
+      NewTopic newTopic = new NewTopic(topicName, 1, (short) 1);
+      topics.add(newTopic);
+    }
+
+    AdminClient kac = AdminClient.create(config());
+    long before = System.currentTimeMillis();
+    kac.createTopics(topics).all().get();
+    System.out.printf("elapsed %d\n", System.currentTimeMillis() - before);
+  }
+
+  @Test
+  public void testDeleteTopics() throws Exception {
+    AdminClient kac = AdminClient.create(config());
+    List<String> topicNames = new ArrayList<>();
+    int count = 1000;
+    for (int i = 0; i < count; i++) {
+      String topicName = String.format("many-%d", i);
+      topicNames.add(topicName);
+    }
+    kac.deleteTopics(topicNames).all().get();
   }
 
   @Test
   public void testSend() throws Exception {
-    try (KafkaProducer<String, String> kp = createProducer();) {
+    try (KafkaProducer<String, String> kp = createProducer(); ) {
       ProducerRecord<String, String> record = new ProducerRecord<>(topic, "key1", "value70");
       Future<RecordMetadata> future = kp.send(record);
       System.out.println("writting...");
@@ -35,18 +75,58 @@ public class ProducerTest {
     }
   }
 
-  public static void main(String[] args) throws Exception {
-    try (KafkaProducer<String, String> kp = createProducer();) {
+  @Test
+  public void testRetries() throws Exception {
+    //    Properties cfg = config();
+    //    cfg.setProperty("retries", "x");
+
+  }
+
+  private static void sendWithCallback() throws Exception {
+    try (KafkaProducer<String, String> kp = createProducer(); ) {
       ProducerRecord<String, String> record = new ProducerRecord<>(topic, "key2", "value2");
-      kp.send(record, new Callback() {
-        @Override
-        public void onCompletion(RecordMetadata metadata, Exception exception) {
-          System.out.printf("topic: %s\n", metadata.topic());
-          System.out.printf("parition: %d\n", metadata.partition());
-          System.out.printf("offset: %d\n", metadata.offset());
-        }
-      });
+      kp.send(
+          record,
+          new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+              System.out.printf("topic: %s\n", metadata.topic());
+              System.out.printf("parition: %d\n", metadata.partition());
+              System.out.printf("offset: %d\n", metadata.offset());
+            }
+          });
     }
   }
 
+  public static void main(String[] args) throws Exception {
+    Properties cfg = config();
+    cfg.setProperty("retries", "1");
+    cfg.setProperty("max.block.ms", "1000");
+    cfg.setProperty("buffer.memory", "1");
+    KafkaProducer<String, String> kp = new KafkaProducer<String, String>(cfg);
+    try {
+      ProducerRecord<String, String> record = new ProducerRecord<>(topic, "key", "++++++++");
+      System.out.printf("sending...\n");
+      kp.send(record);
+      System.out.printf("sent\n");
+      Thread.sleep(1000 * 5);
+    } finally {
+//      kp.close();
+    }
+  }
 }
+//      kp.send(
+//          record,
+//          new Callback() {
+//            @Override
+//            public void onCompletion(RecordMetadata metadata, Exception exception) {
+//              if (exception == null) {
+//                System.out.printf("onCompletion:");
+//                System.out.printf("  topic: %s\n", metadata.topic());
+//                System.out.printf("  partition: %d\n", metadata.partition());
+//                System.out.printf("  offset: %d\n", metadata.offset());
+//              } else {
+//                exception.printStackTrace();
+//              }
+//            }
+//          });
